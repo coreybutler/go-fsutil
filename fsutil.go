ff6cff75d1e3220f131dc042bab64b3b4ed174c6
Corey Butler <corey@coreybutler.com>
2019-08-20 18:25:09 -0500

    Added inline comments for godoc

--- fsutil.go
+++ fsutil.go
@@ -1,5 +1,12 @@
 package fsutil
 
+// The fsutil module provides a lightweight cross-platform
+// set of helper methods for interacting with the file system.
+// Most of the methods are designed with the intention of
+// "guaranteeing" a system resource does or does not exist.
+// They are designed to abstract common functionality into
+// more easily understood code.
+
 import (
 	"os"
 	"path/filepath"
@@ -13,6 +20,17 @@ import (
 // (useful when the filename has not extension).
 // An optional 3rd boolean argument will force the method
 // to treat the path as a directory even if a file extension is present.
+//
+// For example:
+// ```go
+// fsutil.Touch("./path/to/archive.old", false, true)
+// ```
+//
+// Normally, any file path with an extension is determined
+// to be a file. However; the second argument (`false`)
+// instructs the command to **not** force a file. The third
+// argument (`true`) instructs the command to **treat the path
+// like a directory**.
 func Touch(path string, flags ...interface{}) string {
 	abs := Abs(path)
 
@@ -49,12 +67,17 @@ func Touch(path string, flags ...interface{}) string {
 	return abs
 }
 
+// Mkdirp is the equivalent of [mkdir -p](https://en.wikipedia.org/wiki/Mkdir)
+// It will generate the full directory path if it does not already
+// exist.
 func Mkdirp(path string) string {
 	path = Abs(path)
 	os.MkdirAll(path, os.ModePerm)
 	return path
 }
 
+// Exists is a helper method to quickly
+// determine whether a directory or file exists.
 func Exists(path string) bool {
 	if len(Abs(path)) == 0 {
 		return false
@@ -69,11 +92,23 @@ func Exists(path string) bool {
 	return true
 }
 
+// Returns the fully resolved path, even if the
+// path does not exist.
+//
+// ```go
+// fsutil.Abs("./does/not/exist")
+// ```
+// If the code above was run within `/home/user`, the
+// result would be `/home/user/does/not/exist`.
 func Abs(path string) string {
 	abs, _ := filepath.Abs(path)
 	return abs
 }
 
+// Clean will ensure the specified directory exists.
+// If the directory already exists, all of contents
+// are deleted. If the directory does not exist, it
+// is automatically created.
 func Clean(path string) {
 	path = Abs(path)
 
@@ -88,6 +123,8 @@ func Clean(path string) {
 	Mkdirp(path)
 }
 
+// IsFile determines whether the specified path
+// represents a file.
 func IsFile(path string) bool {
 	if !Exists(path) {
 		return false
@@ -101,6 +138,8 @@ func IsFile(path string) bool {
 	return !stat.IsDir()
 }
 
+// IsDirectory determines whether the specified path
+// represents a directory.
 func IsDirectory(path string) bool {
 	if !Exists(path) {
 		return false
