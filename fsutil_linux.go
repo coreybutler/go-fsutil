6f3575294e43e98fbc59e5fcdd23c8fe7f2bb611
coreybutler <770982+coreybutler@users.noreply.github.com>
2025-08-20 22:31:06 -0500

    add better executable checking for posix systems

---
+++ fsutil_linux.go
@@ -0,0 +1,12 @@
+package fsutil
+
+import "os"
+
+func isExecutable(filepath string) bool {
+	info, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	}
+
+	return (info.Mode()&0111 != 0)
+}
